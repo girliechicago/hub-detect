305d72fe04e615e3c5ac0fb29433db9942776985
jrichard <jrichard@blackducksoftware.com>
2017-07-20 11:14:32 -0400

    Cleaning up the classes that are included in the coverage reports

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/SolveMeta.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/SolveMeta.groovy
@@ -20,6 +20,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+package com.blackducksoftware.integration.hub.detect.bomtool.go
+
 import com.google.gson.annotations.SerializedName
 
 class SolveMeta {
